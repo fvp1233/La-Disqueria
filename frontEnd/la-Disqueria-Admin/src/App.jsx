198e67dfb536a2bb3e1388100631a0150ea5786b
nat <natgochez@gmail.com>
2026-04-22 18:47:15 -0600

    dashboard y accesorios

--- frontEnd/la-Disqueria-Admin/src/App.jsx
+++ frontEnd/la-Disqueria-Admin/src/App.jsx
@@ -6,7 +6,8 @@ import DashboardPage from "@/modules/dashboard/pages/DashboardPage"
 import OrdersPage from "@/modules/orders/pages/OrdersPage"
 import DiscsPage from "@/modules/discs/pages/DiscsPage"
 import AddDiscPage from "@/modules/discs/pages/AddDiscPage";
-
+import AccesoriesPage from "@/modules/accesories/pages/AccesoriesPage"
+import AddAccesoryPage from "@/modules/accesories/pages/AddAccesoryPage";
 function App() {
   return (
     <MainLayout>
@@ -14,7 +15,9 @@ function App() {
         <Route path="/" element={<DashboardPage />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/discs" element={<DiscsPage />} />
-        <Route path="/discs/add" element={<AddDiscPage />} /> {/* 🔥 */}
+        <Route path="/discs/add" element={<AddDiscPage />} /> 
+<Route path="/accesories" element={<AccesoriesPage />} />
+<Route path="/accessories/add" element={<AddAccesoryPage />} />
       </Routes>
     </MainLayout>
   )
